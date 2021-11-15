379c824381d204cba11b88e064d2bca95553d477
michaellee8 <ckmichael8@gmail.com>
2021-11-15 22:30:44 +0800

    feat: wip

--- platformio/ArduinoWirelessChargingCar/src/main.cpp
+++ platformio/ArduinoWirelessChargingCar/src/main.cpp
@@ -20,9 +20,12 @@
 #define RIGHT_SONIC_ECHO_PIN A6
 #define RIGHT_SONIC_TRIG_PIN A7
 
+#define SERIAL_BAUD_RATE 115200
+
 // ArduinoSort
 // Simple Insertion sort suitable for running in low memory environment like
-// Arduino grabbed from GitHub
+// Arduino grabbed from GitHub.
+// Just ignore it and skip to line 108.
 
 #ifndef ArduinoSort_h
 #define ArduinoSort_h
@@ -108,8 +111,13 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 // Arduino has no double, double is float
 // float is slow anyway since it is emulated, Arduino has no FPU
-float distance_in_cmL;
-float distance_in_cmR;
+
+// Global sensor readings are here;
+
+float distance_in_cm_L;
+float distance_in_cm_R;
+
+float current_voltage;
 
 #define PWMA 12  // Motor A PWM
 #define DIRA1 34
@@ -149,6 +157,9 @@ float distance_in_cmR;
 // So now we can define some functions for setting PWM (motor speed value)
 // Also perform speed scaling here
 
+// Don't use the below two boilerplate function in your code. They are here for
+// convenience. Use the set_motorX_speed wrappers provided below.
+
 // Don't use this boilerplate function.
 void _set_normal_motor_speed(int dirx1_pin, int dirx2_pin, int pwm_pin,
                              int speed) {
@@ -187,20 +198,99 @@ void _set_reverse_motor_speed(int dirx1_pin, int dirx2_pin, int pwm_pin,
   }
 }
 
+// Skip setting speed if the speed has not changed.
+// We peform state caching here so we don't have to handle it in the logic code.
+
+int motorA_current_speed;
+int motorB_current_speed;
+int motorC_current_speed;
+int motorD_current_speed;
+
 void set_motorA_speed(int speed) {
+  if (motorA_current_speed == speed) {
+    return;
+  }
   _set_normal_motor_speed(DIRA1, DIRA2, PWMA, speed);
+  motorA_current_speed = speed;
 }
 
 void set_motorB_speed(int speed) {
+  if (motorB_current_speed == speed) {
+    return;
+  }
   _set_reverse_motor_speed(DIRB1, DIRB2, PWMB, speed);
+  motorB_current_speed = speed;
 }
 
 void set_motorC_speed(int speed) {
+  if (motorC_current_speed == speed) {
+    return;
+  }
   _set_normal_motor_speed(DIRC1, DIRC2, PWMC, speed);
+  motorC_current_speed = speed;
 }
 
 void set_motorD_speed(int speed) {
-  _set_reverse_motor_speed(DIRD1, DIRD2, PWMD, speed);
+  if (motorD_current_speed == speed) {
+    return;
+  }
+  _set_normal_motor_speed(DIRD1, DIRD2, PWMD, speed);
+  motorD_current_speed = speed;
+}
+
+void check_voltage() {
+  // Subroutine for reading voltage
+  int sensorValue = analogRead(A0);
+  current_voltage = sensorValue * 25.0 / 1023.0;
+}
+
+void measure_distance() {
+  // Measure distance using left and right sonar here.
+  // Measure the distance for SAMPLE_SIZE times, and then take the
+  // average of the middle 3 samples as the final value.
+  // So that we can achieve best accuracy. No need to worry about
+  // time consumed in measuring here since ultrasonic measuring is quick cheap.
+
+  float durationL;
+  float durationR;
+  unsigned long durationLs[SAMPLE_SIZE];
+  unsigned long durationRs[SAMPLE_SIZE];
+
+  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse.
+
+  for (int i = 0; i < SAMPLE_SIZE; i++) {
+    digitalWrite(LEFT_SONIC_TRIG_PIN, LOW);
+    delayMicroseconds(5);
+    digitalWrite(LEFT_SONIC_TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(LEFT_SONIC_TRIG_PIN, LOW);
+    durationLs[i] = pulseIn(RIGHT_SONIC_ECHO_PIN, HIGH);
+
+    // Take some rest before we measure the right sensor.
+    delayMicroseconds(20);
+
+    digitalWrite(RIGHT_SONIC_ECHO_PIN, LOW);
+    delayMicroseconds(5);
+    digitalWrite(RIGHT_SONIC_TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(RIGHT_SONIC_TRIG_PIN, LOW);
+    durationRs[i] = pulseIn(RIGHT_SONIC_ECHO_PIN, HIGH);
+  }
+
+  sortArray(durationLs, SAMPLE_SIZE);
+  sortArray(durationRs, SAMPLE_SIZE);
+
+  durationL = durationLs[SAMPLE_SIZE / 2 - 1] + durationLs[SAMPLE_SIZE / 2] +
+              durationLs[SAMPLE_SIZE / 2 + 1];
+  durationL /= 3.0;
+
+  durationR = durationRs[SAMPLE_SIZE / 2 - 1] + durationRs[SAMPLE_SIZE / 2] +
+              durationRs[SAMPLE_SIZE / 2 + 1];
+  durationR /= 3.0;
+
+  distance_in_cm_L = durationL / 2.0 / 29.1;
+  distance_in_cm_R = durationR / 2.0 / 29.1;
 }
 
 void setup() {

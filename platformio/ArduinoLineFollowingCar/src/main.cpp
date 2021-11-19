32a86aa896ec718a01b260d7c351cfbc99b555d6
michaellee8 <ckmichael8@gmail.com>
2021-11-19 20:47:22 +0800

    feat: wip

--- platformio/ArduinoLineFollowingCar/src/main.cpp
+++ platformio/ArduinoLineFollowingCar/src/main.cpp
@@ -25,13 +25,21 @@ float distance_in_cm_R;
 
 TimedState force_forward_state(500);
 
-TimedState turning_state(3000);
+TimedState y_intersection_left_turning_state(3000);
+
+TimedState y_intersection_right_turning_state(3000);
 
 TimedState nothing_seen_continue_orignial_direciton_state(1000);
 
 TimedState nothing_seen_back_state(2000);
 
+TimedState t_intersection_turning_state(3000);
+
 State got_l, got_m, got_r;
+
+State t_intersection_cleared;
+
+State t_got_l, t_got_m, t_got_r;
 
 // A number variable used for debugging that will be printed on log.
 // Here we use it to log application state;
@@ -215,10 +223,87 @@ void RunLogic() {
     return;
   }
 
-  if (turning_state.IsInside()) {
+  if (t_intersection_turning_state.IsInside()) {
+    if (is_l_black || is_r_black) {
+      // We are not cleard from the intersection yet.
+      debug_number = 25;
+      MoveForward(FORWARD_SPEED);
+      return;
+    }
+    if (is_m_black) {
+      debug_number = 26;
+      t_intersection_cleared.Enter();
+      return;
+    }
+    if (!t_intersection_cleared.IsInside()) {
+      // Impossible case!
+      debug_number = 27;
+      RunMotor(0, 0, 0, 0);
+      return;
+    }
+    // We are cleared! Let's do the rotation.
     if (intended_direction == Direction::kForward) {
+      debug_number = 28;
+      // To move forward, just exit the state;
+      t_intersection_turning_state.Exit();
+      t_intersection_cleared.Exit();
+      t_got_l.Exit();
+      t_got_m.Exit();
+      t_got_r.Exit();
+      return;
+    }
+
+    if (intended_direction == Direction::kLeft) {
+      if (is_l_black) {
+        t_got_l.Enter();
+      }
+      if (t_got_l.IsInside() && is_m_black) {
+        t_got_m.Enter();
+      }
+      if (t_got_l.IsInside() && t_got_m.IsInside()) {
+        t_intersection_turning_state.Exit();
+        t_got_l.Exit();
+        t_got_m.Exit();
+        t_intersection_cleared.Exit();
+        debug_number = 29;
+        return;
+      } else {
+        RunMotor(-ROTATION_SPEED, -ROTATION_SPEED, ROTATION_SPEED,
+                 ROTATION_SPEED);
+        debug_number = 30;
+        return;
+      }
+    }
+
+    if (intended_direction == Direction::kRight) {
       if (is_r_black) {
-        RunMotor(ROTATION_SPEED, ROTATION_SPEED, -ROTATION_SPEED, -ROTATION_SPEED);
+        t_got_r.Enter();
+      }
+      if (t_got_r.IsInside() && is_m_black) {
+        t_got_m.Enter();
+      }
+      if (t_got_r.IsInside() && t_got_m.IsInside()) {
+        t_intersection_turning_state.Exit();
+        t_got_r.Exit();
+        t_got_m.Exit();
+        t_intersection_cleared.Exit();
+        debug_number = 31;
+        return;
+      } else {
+        RunMotor(ROTATION_SPEED, ROTATION_SPEED, -ROTATION_SPEED,
+                 -ROTATION_SPEED);
+        debug_number = 32;
+        return;
+      }
+    }
+  }
+
+  if (y_intersection_left_turning_state.IsInside()) {
+    if (intended_direction == Direction::kForward ||
+        intended_direction == Direction::kRight) {
+      if (is_r_black) {
+        RunMotor(ROTATION_SPEED, ROTATION_SPEED, -ROTATION_SPEED,
+                 -ROTATION_SPEED);
         debug_number = 12;
         return;
       } else {
@@ -240,23 +325,78 @@ void RunLogic() {
         got_m.Enter();
       }
       if (got_m.IsInside() && got_r.IsInside()) {
-        turning_state.Exit();
+        y_intersection_left_turning_state.Exit();
         got_m.Exit();
         got_r.Exit();
         force_forward_state.Enter();
         debug_number = 15;
         return;
       } else {
-        RunMotor(-ROTATION_SPEED, -ROTATION_SPEED, ROTATION_SPEED, ROTATION_SPEED);
+        RunMotor(-ROTATION_SPEED, -ROTATION_SPEED, ROTATION_SPEED,
+                 ROTATION_SPEED);
         debug_number = 16;
         return;
       }
     }
   }
 
-  if (is_m_black && is_l_black) {
-    turning_state.Enter();
+  if (y_intersection_right_turning_state.IsInside()) {
+    if (intended_direction == Direction::kForward ||
+        intended_direction == Direction::kLeft) {
+      if (is_r_black) {
+        RunMotor(ROTATION_SPEED, ROTATION_SPEED, -ROTATION_SPEED,
+                 -ROTATION_SPEED);
+        debug_number = 12;
+        return;
+      } else {
+        if (is_m_black) {
+          MoveForward(FORWARD_SPEED);
+          debug_number = 13;
+          return;
+        } else {
+          MoveForward(FORWARD_SPEED);
+          debug_number = 14;
+          return;
+        }
+      }
+    } else if (intended_direction == Direction::kRight) {
+      if (is_l_black) {
+        got_l.Enter();
+      }
+      if (got_l.IsInside() && !is_r_black && is_m_black) {
+        got_m.Enter();
+      }
+      if (got_m.IsInside() && got_r.IsInside()) {
+        y_intersection_left_turning_state.Exit();
+        got_m.Exit();
+        got_l.Exit();
+        force_forward_state.Enter();
+        debug_number = 29;
+        return;
+      } else {
+        RunMotor(ROTATION_SPEED, ROTATION_SPEED, -ROTATION_SPEED,
+                 -ROTATION_SPEED);
+        debug_number = 30;
+        return;
+      }
+    }
+  }
+
+  if (is_l_black && is_m_black && is_r_black) {
+    t_intersection_turning_state.Enter();
+    debug_number = 24;
+    return;
+  }
+
+  if (is_m_black && is_l_black && !t_intersection_turning_state.IsInside()) {
+    y_intersection_left_turning_state.Enter();
     debug_number = 17;
+    return;
+  }
+
+  if (is_m_black && is_r_black && !t_intersection_turning_state.IsInside()) {
+    y_intersection_right_turning_state.Enter();
+    debug_number = 31;
     return;
   }
 
@@ -284,12 +424,14 @@ void RunLogic() {
   // All sensors had no input.
   switch (last_seen_side) {
     case Side::kLeft:
-      RunMotor(-ROTATION_SPEED, -ROTATION_SPEED, ROTATION_SPEED, ROTATION_SPEED);
+      RunMotor(-ROTATION_SPEED, -ROTATION_SPEED, ROTATION_SPEED,
+               ROTATION_SPEED);
       debug_number = 21;
       break;
 
     case Side::kRight:
-      RunMotor(ROTATION_SPEED, ROTATION_SPEED, -ROTATION_SPEED, -ROTATION_SPEED);
+      RunMotor(ROTATION_SPEED, ROTATION_SPEED, -ROTATION_SPEED,
+               -ROTATION_SPEED);
       debug_number = 22;
       break;
 
